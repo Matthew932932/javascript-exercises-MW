451a1208ca6971c2e4e927c85b857f0ff9569133
Matthew932932 <184472697+Matthew932932@users.noreply.github.com>
2024-10-28 10:41:43 +1100

    exercise 2 changes and new files

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(Mword, Mtimes) {
+    if (Mtimes < 0) return "ERROR";
+    let mstr = "";
+    
+    for(let i = 0; i < Mtimes; i++)
+    {
+        mstr += Mword;
+    }
+    return mstr;
 };
 
 // Do not edit below this line
