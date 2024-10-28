8ee37c63144b1766863127c04e43bd856baf46fa
Matthew932932 <184472697+Matthew932932@users.noreply.github.com>
2024-10-28 11:02:15 +1100

    exercise 3 changes and new files

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,14 @@
-const reverseString = function() {
+const reverseString = function(Mstr) {
+
+    let splStr = "";
+    let revArr = "";
+    let joinArr = "";
+
+    splStr = Mstr.split("");
+    revArr = splStr.reverse();
+    joinArr = revArr.join("");
+
+    return joinArr;
 
 };
 
