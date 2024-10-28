1ee6f56741362e1d11a55c34ae8d574af3685516
Matthew932932 <184472697+Matthew932932@users.noreply.github.com>
2024-10-28 15:21:19 +1100

    exercise 5 changes and new files

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,32 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    
+    let result = 0;
 
+    if(num1 < 0 || num2 < 0 || !(Number.isInteger(num1)) || !(Number.isInteger(num2)))
+    {
+        return "ERROR";
+    }
+
+
+    if(num1<num2)
+    {
+        result=num1;  
+        for(i=num1; i<(num2); i++)
+        {
+            result = result + (i+1);
+        }
+    }
+    else if(num2<num1)
+        {
+            result=num2;  
+            for(i=num2; i<(num1); i++)
+            {
+                result = result + (i+1);
+            }
+        }
+
+
+    return result;
 };
 
 // Do not edit below this line
