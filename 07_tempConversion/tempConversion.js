31a887d1e3c250612176d12a8a57d0a313d4dbc8
Matthew932932 <184472697+Matthew932932@users.noreply.github.com>
2024-10-28 16:37:30 +1100

    exercise 07 changes and new files

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num1) {
+
+  num1 = Math.round(((num1-32)*(5/9))*10) / 10;
+  return num1;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num2) {
+  
+  num2 = Math.round((num2 * 9/5 + 32)*10)/10; 
+  return num2;
 };
 
 // Do not edit below this line
