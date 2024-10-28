b0fd70418e1a007e1a51ba22a3843150c1f550e2
Matthew932932 <184472697+Matthew932932@users.noreply.github.com>
2024-10-28 14:47:37 +1100

    exercise 4 changes and new files

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(mwArr, ...args) {
+    let newArr = [];
+
+    mwArr.forEach(item => {
+        if (!args.includes(item)) {
+            newArr.push(item);
+          }
+    });
+
+    return newArr;
 };
 
 // Do not edit below this line
