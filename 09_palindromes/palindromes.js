1c5e42f3f02389ae444e56fa983c2516070f6d42
Matthew932932 <184472697+Matthew932932@users.noreply.github.com>
2024-11-07 15:17:04 +1100

    exercise 09 complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+ 
+    const cleanedString = str.toLowerCase()
+    .split('')
+    .filter((character) => alphanumerical.includes(character))
+    .join('');
 
+    const newstr = cleanedString.split('')
+    .reverse()
+    .join('');
+
+
+    return newstr === cleanedString;
+    
 };
 
 // Do not edit below this line
