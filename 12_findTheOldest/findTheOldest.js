98be49d7e4e87601552e8f8b172252214b57e5cd
Matthew932932 <184472697+Matthew932932@users.noreply.github.com>
2024-11-07 16:58:43 +1100

    exercise 12 completed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const getAge = function(person)
+{
 
+    if (!person.yearOfDeath) {
+        person.yearOfDeath = new Date().getFullYear();
+    }
+
+    return person.yearOfDeath - person.yearOfBirth;
+
+}
+
+const findTheOldest = function(people) {
+    const ages = people.toSorted((person, nextPerson) => 
+        getAge(nextPerson) - getAge(person)
+    );
+    
+    const oldest =ages[0];
+    return oldest;
 };
 
 // Do not edit below this line
