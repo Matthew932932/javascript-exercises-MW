dce698930926a39a2ac8c669850a459e52378816
Matthew932932 <184472697+Matthew932932@users.noreply.github.com>
2024-11-07 16:00:19 +1100

    exercise 10 complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,28 @@
-const fibonacci = function() {
+const fibonacci = function(numof) {
+
+    let count
+    if (typeof numof !== 'number') {
+        count = parseInt(numof);
+    } else {
+        count = numof;
+    }
+
+    
+    if(numof < 0)
+    {return "OOPS";}
+    else if(count === 0)
+    {return 0;}
+    
+    firstPrev = 1;
+    secondPrev = 0;
+    
+    for (let i = 2; i <= numof; i++) {
+        let curr = firstPrev + secondPrev;
+        secondPrev = firstPrev;
+        firstPrev = curr;
+    }
+
+    return firstPrev;
 
 };
 
